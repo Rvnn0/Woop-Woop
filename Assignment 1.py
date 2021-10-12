8fbbfbe70aec83fe475343be89199f69ad85e34b
Angelo Lopez <1090925@learn.vsb.bc.ca>
2021-10-12 14:31:52 -0700

    added Woop Woop Woop.py

---
+++ Assignment 1.py
@@ -0,0 +1,19 @@
+# Angelo. L
+# Programming 11
+# September 10th 2021
+# ChatBot Assignment
+
+import random
+
+myinput = input("What is your favourite food? ")
+response_list = [ "Oooo alright", "ehh not the greatest", "sounds good"]
+print(random.choice(response_list))
+
+myinput = input("What is your favourite Video Game?  ")
+response_list = [ "Sheeshhhh",  "Ew.... tf", "Okay okay"]
+print(random.choice(response_list))
+
+myinput = input("Favourite Song?")
+response_list = [ "Haven't heard of this", "Dope Dope... ", "Eh not to bad"]
+print(random.choice(response_list))
+#All of these commands ask a different question and put out a random response
